1897bfc4eb81596f157bb8dbd12734083be8b010
Rivaldy <airwalk9919@gmail.com>
2026-08-24 17:30:29 +0700

    feat: update FastAPI main application

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+products = [
+    {"id": 1, "name": "Keyboard", "price": 500000},
+    {"id": 2, "name": "Mouse", "price": 250000}
+]
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI is running"}
+
+@app.get("/products")
+def get_products():
+    return products
